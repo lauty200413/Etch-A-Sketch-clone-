d959765f936da31de7b759fa6b6778a681644322
lauty200413 <85744443+lauty200413@users.noreply.github.com>
2023-10-29 07:48:14 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from turtle import Turtle, Screen
+
+
+my_screen = Screen()
+merki = Turtle()
+
+
+def move_forwards():
+    merki.forward(10)
+
+
+def move_backwards():
+    merki.forward(-10)
+
+
+def turn_clockwise():
+    new_heading = merki.heading() + 10
+    merki.setheading(new_heading)
+
+
+def turn_counter_clockwise():
+    new_heading = merki.heading() - 10
+    merki.setheading(new_heading)
+
+
+def clear():
+    merki.goto(0, 0)
+    merki.setheading(0)
+    merki.clear()
+
+
+my_screen.listen()
+my_screen.onkey(key="w", fun=move_forwards)
+my_screen.onkey(key="s", fun=move_backwards)
+my_screen.onkey(key="a", fun=turn_counter_clockwise)
+my_screen.onkey(key="d", fun=turn_clockwise)
+my_screen.onkey(key="space", fun=clear)
+
+
+my_screen.exitonclick()
